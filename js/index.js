6f9fba7799eef4a54f76e106b65403a9ccdd5f90
Javier <jdpledezma@gmail.com>
2024-05-26 21:57:20 -0400

    First Commit.Add Project

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const btnSmallMenu = document.querySelector(".btn-small-menu")
+const smallMenu = document.querySelector(".small-menu")
+const btnMenuItem = document.querySelectorAll(".item")
+let menuActivated = false
+
+
+btnSmallMenu.addEventListener("click",()=>{
+    if(menuActivated == false){
+    smallMenu.classList.add("open")
+    btnSmallMenu.src = "./img/exit.svg";
+    smallMenu.style.display = "flex";
+    menuActivated = true;
+    }else{
+        smallMenu.classList.remove("open")
+        menuActivated = false;
+        btnSmallMenu.src = "./img/hamburguer.svg";
+    }
+})
+
+btnMenuItem.forEach((btn) =>{
+    btn.addEventListener("click",()=>{
+        smallMenu.style.display = "none";
+        menuActivated = false;
+        btnSmallMenu.src = "./img/hamburguer.svg";
+    })
+})
+
